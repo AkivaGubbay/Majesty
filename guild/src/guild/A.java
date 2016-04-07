8bfb4877252f9d124d2c9bb92b846f817751854e
Ezra <Ezra@Ezra_Gubbay>
2016-04-07 20:43:32 +0300

    Finally made a successful commit and push

--- guild/src/guild/A.java
+++ guild/src/guild/A.java
@@ -4,7 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hi Ezra!");
-		System.out.println("Trying to push");
+		System.out.println("This is a successful push :)");
 
 	}
 
